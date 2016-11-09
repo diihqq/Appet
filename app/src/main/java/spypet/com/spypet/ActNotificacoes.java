2b19ce719c249dee55ed75fe69194911c111a813
diihqq <diogo_esteves@live.com>
2016-11-09 18:05:43 -0200

    Alteração Notificações

--- app/src/main/java/spypet/com/spypet/ActNotificacoes.java
+++ app/src/main/java/spypet/com/spypet/ActNotificacoes.java
@@ -15,9 +15,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -30,6 +33,7 @@ import java.util.Locale;
 
 import controlador.GerenciadorSharedPreferences;
 import controlador.Requisicao;
+import controlador.TransformacaoCirculo;
 import modelo.Animal;
 import modelo.Mensagem;
 import modelo.Notificacao;
@@ -47,6 +51,13 @@ public class ActNotificacoes extends AppCompatActivity{
     String animal = "";
     String lat = "";
     String lot = "";
+    TextView tvData;
+    TextView tvPet;
+    TextView tvMensagem;
+    TextView tvContato;
+    TextView tvContatoLabel;
+    ImageView ivFotoPet;
+    ImageView ivTipoNotificacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,19 +78,57 @@ public class ActNotificacoes extends AppCompatActivity{
                 if (convertView == null)
                     convertView = getLayoutInflater().inflate(R.layout.item_notificacoes, null); /* obtém o objeto que está nesta posição do ArrayAdapter */
 
-                TextView mensagem = (TextView) convertView.findViewById(R.id.tvMensagem);
-                Notificacao notificacao = getItem(position);
+                tvData = (TextView) convertView.findViewById(R.id.tvData);
+                tvPet = (TextView) convertView.findViewById(R.id.tvPet);
+                tvMensagem = (TextView) convertView.findViewById(R.id.tvMensagem);
+                tvContato = (TextView) convertView.findViewById(R.id.tvContato);
+                tvContatoLabel = (TextView) convertView.findViewById(R.id.tvContatoLabel);
+                ivTipoNotificacao = (ImageView)convertView.findViewById(R.id.ivTipoNotificacao);
 
-                if (!notificacao.getMensagem().startsWith("Localizado por "))
+                //Recebe mensagem
+                Notificacao notificacao = getItem(position);
+                String mensagem = notificacao.getMensagem();
+
+                //Se não for mensagem manual
+                if (!notificacao.getMensagem().contains("foi localizado pelo usuário"))
                 {
+
+                    //Seu pet xx foi visto na data xx/xx/xxxx hh:mm:ss em xxx.
+                    //As coordenadas são latitude = xx e longitude = xx.
+
                     //Troca o valor 'As coordenadas são latitude = x e longitude = x' por outra mensagem.
                     String msg_notificacao_quebra = notificacao.getMensagem().substring(0, notificacao.getMensagem().indexOf("As coordenadas"));
                     msg_notificacao_quebra += "Clique aqui para ver a localização no mapa." ;
 
-                    mensagem.setText(msg_notificacao_quebra);
+                    Picasso.with(getContext()).load(R.drawable.mapa).into(ivTipoNotificacao);
+
+                    //Altera o label e insere mensagem de localização
+                    tvContatoLabel.setText("Localização:");
+                    tvContato.setText("Clique aqui para ver o local no mapa.");
+
+                    //Recupera cidade onde o pet foi localizado
+                    tvMensagem.setText("Pet visto em " + msg_notificacao_quebra.substring(msg_notificacao_quebra.indexOf("em") + 3,
+                            msg_notificacao_quebra.indexOf(". Clique")));
                 }
                 else
-                    mensagem.setText(notificacao.getMensagem());
+                {
+                    //Seu pet X foi localizado pelo usuário X na data xx/xx/xxxx hh:mm:ss
+                    //Mensagem: xx
+                    //Contato: xxxx-xxxx
+
+                    Picasso.with(getContext()).load(R.drawable.mensagem).into(ivTipoNotificacao);
+
+                    //Recupera mensagem:
+                    tvMensagem.setText(mensagem.substring(mensagem.indexOf("Mensagem:") + 10, mensagem.indexOf("\nContato:") - 1));
+
+                    //Recupera nome do usuário + contato de telefone
+                    tvContato.setText(mensagem.substring(mensagem.indexOf("usuário") + 8, mensagem.indexOf("na data") - 1) + " - " +
+                            mensagem.substring(mensagem.indexOf("Contato:") + 9, mensagem.length()));
+                }
+
+                //Recupera nome de pet e data de notificação
+                tvPet.setText(mensagem.substring(mensagem.indexOf("pet") + 4, mensagem.indexOf("foi")));
+                tvData.setText(mensagem.substring(mensagem.indexOf("data") + 5, mensagem.indexOf("data") + 5 + 19));
 
                 //Se a notificação ja foi lida muda a cor do fundo
                 if(notificacao.isLida()) {
@@ -99,7 +148,7 @@ public class ActNotificacoes extends AppCompatActivity{
 
                 Notificacao notificacao = (Notificacao) parent.getItemAtPosition(position);
 
-                if (!notificacao.getMensagem().startsWith("Localizado por ")) {
+                if (!notificacao.getMensagem().contains("foi localizado pelo usuário")) {
 
                     //Recupera as informações animal, latitude e longitude
                     String msg_notificacao = notificacao.getMensagem();
